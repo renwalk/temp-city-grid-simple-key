eeafc60b68733fb611edbfd30417c7c5180c505f
Renee Walker <54253764+renwalk@users.noreply.github.com>
2024-06-11 15:24:55 -0400

    Update sketch.js
    changed to no background with light text

--- sketch.js
+++ sketch.js
@@ -1,6 +1,16 @@
 var url = "https://api.openweathermap.org/data/2.5/weather?q=";
 var apikey = "&units=imperial&appid=ecf071a12fde88e534d97674aa7cd83b";
-var cityNames = ["PHILADELPHIA", "BOSTON", "PHOENIX", "SEATTLE", "PARIS", "NEW YORK", "LOS ANGELES", "DUBAI", "MELBOURNE"];
+var cityNames = [
+  "PHILADELPHIA",
+  "BOSTON",
+  "PHOENIX",
+  "SEATTLE",
+  "PARIS",
+  "NEW YORK",
+  "LOS ANGELES",
+  "DUBAI",
+  "MELBOURNE",
+];
 var weatherData = []; // Array to store the weather data for each city
 
 // setting up my high and low colors for my color scale
@@ -12,8 +22,8 @@ var padding = 10;
 
 function setup() {
   // Set a fixed canvas size to fit your desired page layout
-  var canvasWidth = 1000; // Adjust the canvas width as needed
-  var canvasHeight = 700;
+  var canvasWidth = 1050; // Adjust the canvas width as needed
+  var canvasHeight = 680;
   createCanvas(canvasWidth, canvasHeight);
   rectMode(CENTER);
   noStroke();
@@ -22,27 +32,25 @@ function setup() {
   // Load weather data for each city
   var promises = cityNames.map((cityName) => {
     var request = url + cityName + apikey;
-    return fetch(request)
-      .then((response) => response.json());
+    return fetch(request).then((response) => response.json());
   });
 
-  Promise.all(promises)
-    .then((data) => {
-      weatherData = data;
-      drawRectangles(); // Draw the rectangles once data is loaded
-      drawTemperatureKeyAndScale(); // Draw the temperature key and scale
-    });
+  Promise.all(promises).then((data) => {
+    weatherData = data;
+    drawRectangles(); // Draw the rectangles once data is loaded
+    drawTemperatureKeyAndScale(); // Draw the temperature key and scale
+  });
 }
 
 function drawRectangles() {
   var totalCities = cityNames.length;
   var numberOfColumns = 3; // Total columns, including the sidebar
   var numberOfRows = Math.ceil(totalCities / numberOfColumns);
-  var rectangleWidth = (width * 3 / 4) / numberOfColumns; // Adjust the width of the sidebar
+  var rectangleWidth = (width * 3) / 4 / numberOfColumns; // Adjust the width of the sidebar
   var rectangleHeight = height / numberOfRows;
 
   // Calculate the width of the sidebar
-  var sidebarWidth = (width / 4 + 20);
+  var sidebarWidth = width / 4 + 20;
 
   // Use the loaded weather data to display each city's rectangle
   for (var i = 0; i < numberOfRows; i++) {
@@ -58,11 +66,16 @@ function drawRectangles() {
       var c = lerpColor(color(lowColor), color(highColor), s);
       fill(c);
 
-      var positionX = (j * rectangleWidth) + (width / 4 + 20); // Adjust X position for the sidebar
+      var positionX = j * rectangleWidth + (width / 4 + 20); // Adjust X position for the sidebar
       var positionY = i * rectangleHeight;
       var offset = rectangleWidth / 2;
 
-      rect(positionX + offset, positionY + offset, rectangleWidth, rectangleHeight);
+      rect(
+        positionX + offset,
+        positionY + offset,
+        rectangleWidth,
+        rectangleHeight
+      );
 
       // Creates 'feels like' circle in the middle
       push();
@@ -80,56 +93,61 @@ function drawRectangles() {
 
   // Draw the sidebar on the left
   blendMode(BLEND);
-  fill('#F7F7F1'); // Change the fill color to white for the sidebar
+  noFill(); // Change the fill color for the sidebar
   rect(sidebarWidth / 2, height / 2, sidebarWidth, height);
 }
 
 function drawTemperatureKeyAndScale() {
   // Sidebar
   // Temp Key
-  var keySize = width / 4 - margin*2;
+  var keySize = width / 4 - margin * 2;
   var offset = keySize / 2;
   var yPosition = height / 5.5;
-
 
   // Temp Scale
   var scaleCircle = 20;
   var scaleOffset = scaleCircle / 4;
 
-  for (let i = 0; i <= 100; i += 10) { // go up in 10% increments
+  for (let i = 0; i <= 100; i += 10) {
+    // go up in 10% increments
     let s2 = i / 100;
     let c3 = lerpColor(color(lowColor), color(highColor), s2);
 
     fill(c3);
-    ellipse(margin + scaleOffset + i * 2, height - 65, scaleCircle, scaleCircle);
+    ellipse(
+      margin + scaleOffset + i * 2,
+      height - 65,
+      scaleCircle,
+      scaleCircle
+    );
   }
 
-  
   for (let i = 0; i <= 100; i += 10) {
-    fill(0);
+    fill(255);
     textSize(9);
     textAlign(LEFT);
-    text(i, margin + (i * 2), height - 40);
+    text(i, margin + i * 2, height - 40);
   }
 
   // Draw the key box and labels
-  var textScale = 10
-  
-  textSize(textScale);
-  text('TEMPERATURE SCALE', margin, height - 85);
+  var textScale = 10;
 
-  stroke(120);
+  textSize(textScale);
+  text("TEMPERATURE SCALE", margin, height - 85);
+
+  stroke(220);
   strokeWeight(0.75);
   noFill();
   rect(margin + keySize / 2, yPosition, keySize, keySize);
-  ellipse(margin + keySize / 2, yPosition, keySize-50, keySize-50);
+  ellipse(margin + keySize / 2, yPosition, keySize - 50, keySize - 50);
 
   textAlign(CENTER, CENTER);
-  fill(0);
+  fill(255);
+  noStroke();
   textSize(textScale);
-  text('ACTUAL', margin + offset, yPosition - offset + keySize*.05);
-  text('FEELS LIKE', margin + offset, yPosition - offset + keySize * 0.5);
-  
+  text("ACTUAL", margin + offset, yPosition - offset + keySize * 0.05);
+  text("FEELS LIKE", margin + offset, yPosition - offset + keySize * 0.5);
+
   // City Key
   var cityKeyX = margin + offset * 0.35; // Adjust the X position for the city key
   var cityKeyY = yPosition - offset + keySize * 1.5; // Adjust the Y position for the city key
@@ -152,26 +170,37 @@ function drawTemperatureKeyAndScale() {
     var cellX = cityKeyX + col * cellWidth;
     var cellY = cityKeyY + row * cellHeight;
 
-    fill(0);
-    textSize(textScale*.75)
+    push();
+    fill(255);
+    noStroke();
+    textSize(textScale * 0.75);
     textAlign(CENTER, CENTER);
-    
+
     // City Labels
     text(cityNameText, cellX, cellY);
     textAlign(LEFT);
+    pop();
 
     // Draw vertical lines to denote columns (except for the first column)
     if (col > 0) {
-      stroke(120);
+      stroke(220);
       strokeWeight(0.75);
-      line(cellX-cellWidth/2, cityKeyY-cellHeight/2, cellX-cellWidth/2, cityKeyY + keySize-cellHeight/2);
+      line(
+        cellX - cellWidth / 2,
+        cityKeyY - cellHeight / 2,
+        cellX - cellWidth / 2,
+        cityKeyY + keySize - cellHeight / 2
+      );
     }
 
     // Draw horizontal lines to denote rows (except for the first row)
     if (row > 0) {
-      stroke(120);
-      strokeWeight(0.75);
-      line(cityKeyX-cellWidth/2, cellY-cellHeight/2, cityKeyX + keySize-cellWidth/2, cellY-cellHeight/2);
+      line(
+        cityKeyX - cellWidth / 2,
+        cellY - cellHeight / 2,
+        cityKeyX + keySize - cellWidth / 2,
+        cellY - cellHeight / 2
+      );
     }
   }
 }
